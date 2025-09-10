251d92a67bd8c56cdca93845e041bf1fb8655a3d
eaindome <ekow.a.indome@gmail.com>
2025-09-10 09:51:59 +0000

    update: deleting fido2 passkey functionalities

--- Domurion/src/Domurion/Services/Interfaces/IFidoCredentialService.cs
+++
@@ -1,9 +0,0 @@
-using Domurion.Models;
-
-namespace Domurion.Services.Interfaces
-{
-    public interface IFidoCredentialService
-    {
-        List<FidoCredential> GetCredentialsByUserId(Guid userId);
-    }
-}
