0972e7971589683d7d0305871c82e64159ba8052
eaindome <ekow.a.indome@gmail.com>
2025-09-10 09:47:54 +0000

    add: Audit log helper

---
+++ Domurion/src/Domurion/Helpers/AuditLogger.cs
@@ -0,0 +1,23 @@
+using Domurion.Models;
+
+namespace Domurion.Helpers
+{
+    public static class AuditLogger
+    {
+        public static void Log(Domurion.Data.DataContext context, Guid userId, string username, Guid? credentialId, string action, string? ipAddress, string? site = null)
+        {
+            var log = new AuditLog
+            {
+                UserId = userId,
+                Username = username,
+                CredentialId = credentialId,
+                Action = action,
+                Timestamp = DateTime.UtcNow,
+                IpAddress = ipAddress ?? string.Empty,
+                Site = site
+            };
+            context.AuditLogs.Add(log);
+            context.SaveChanges();
+        }
+    }
+}
