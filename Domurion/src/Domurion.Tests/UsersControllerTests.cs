f3400c65d13e6ada47188b331f014f4c9d78b22e
eaindome <ekow.a.indome@gmail.com>
2025-09-10 12:52:02 +0000

    update: completing user controller test cases version 3

--- Domurion/src/Domurion.Tests/UsersControllerTests.cs
+++ Domurion/src/Domurion.Tests/UsersControllerTests.cs
@@ -105,5 +105,137 @@ namespace Domurion.Tests
             Assert.Contains("Invalid username or password", unauthorized.Value.ToString());
         }
         #endregion
+
+        #region Update
+        [Fact]
+        public void Update_Success_ChangesUsername()
+        {
+            var context = CreateInMemoryContext();
+            var controller = CreateController(context);
+            var regResult = controller.Register(new UserDto { Username = "olduser", Password = "StrongP@ssw0rd!" });
+            var user = Assert.IsType<OkObjectResult>(regResult);
+            Assert.NotNull(user.Value);
+            var idProp = user.Value?.GetType().GetProperty("Id");
+            Assert.NotNull(idProp);
+            var idObj = idProp.GetValue(user.Value, null);
+            Assert.NotNull(idObj);
+            var userId = (Guid)idObj;
+            var result = controller.Update(userId, "newuser", null);
+            var ok = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(ok.Value);
+            var usernameProp = ok.Value.GetType().GetProperty("Username");
+            Assert.NotNull(usernameProp);
+            var usernameObj = usernameProp.GetValue(ok.Value, null);
+            Assert.NotNull(usernameObj);
+            Assert.Equal("newuser", usernameObj);
+        }
+
+        [Fact]
+        public void Update_DuplicateUsername_ReturnsConflict()
+        {
+            var context = CreateInMemoryContext();
+            var controller = CreateController(context);
+            var regResult1 = controller.Register(new UserDto { Username = "user1", Password = "StrongP@ssw0rd!" });
+            var regResult2 = controller.Register(new UserDto { Username = "user2", Password = "StrongP@ssw0rd!" });
+            var user2 = Assert.IsType<OkObjectResult>(regResult2);
+            Assert.NotNull(user2.Value);
+            var idProp = user2.Value?.GetType().GetProperty("Id");
+            Assert.NotNull(idProp);
+            var idObj = idProp.GetValue(user2.Value, null);
+            Assert.NotNull(idObj);
+            var user2Id = (Guid)idObj;
+            var result = controller.Update(user2Id, "user1", null);
+            var conflict = Assert.IsType<ConflictObjectResult>(result);
+            Assert.NotNull(conflict.Value);
+            Assert.Contains("already exists", conflict.Value.ToString());
+        }
+
+        [Fact]
+        public void Update_WeakPassword_ReturnsBadRequest()
+        {
+            var context = CreateInMemoryContext();
+            var controller = CreateController(context);
+            var regResult = controller.Register(new UserDto { Username = "user", Password = "StrongP@ssw0rd!" });
+            var user = Assert.IsType<OkObjectResult>(regResult);
+            Assert.NotNull(user.Value);
+            var idProp = user.Value?.GetType().GetProperty("Id");
+            Assert.NotNull(idProp);
+            var idObj = idProp.GetValue(user.Value, null);
+            Assert.NotNull(idObj);
+            var userId = (Guid)idObj;
+            var result = controller.Update(userId, null, "weak");
+            var badRequest = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.NotNull(badRequest.Value);
+            Assert.Contains("strength", badRequest.Value.ToString());
+        }
+
+        [Fact]
+        public void Update_PasswordReuse_ReturnsBadRequest()
+        {
+            var context = CreateInMemoryContext();
+            var controller = CreateController(context);
+            var regResult = controller.Register(new UserDto { Username = "user", Password = "StrongP@ssw0rd!" });
+            var user = Assert.IsType<OkObjectResult>(regResult);
+            Assert.NotNull(user.Value);
+            var idProp = user.Value?.GetType().GetProperty("Id");
+            Assert.NotNull(idProp);
+            var idObj = idProp.GetValue(user.Value, null);
+            Assert.NotNull(idObj);
+            var userId = (Guid)idObj;
+            // Change password 5 times
+            for (int i = 0; i < 5; i++)
+            {
+                controller.Update(userId, null, $"NewStr0ngP@ss{i}!");
+            }
+            // Try to reuse original password
+            var result = controller.Update(userId, null, "StrongP@ssw0rd!");
+            var badRequest = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.NotNull(badRequest.Value);
+            Assert.Contains("reuse", badRequest.Value.ToString(), StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public void Update_UserNotFound_ReturnsNotFound()
+        {
+            var context = CreateInMemoryContext();
+            var controller = CreateController(context);
+            var missingId = Guid.NewGuid();
+            var result = controller.Update(missingId, "newuser", "StrongP@ssw0rd!");
+            var notFound = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.NotNull(notFound.Value);
+            Assert.Contains("not found", notFound.Value.ToString(), StringComparison.OrdinalIgnoreCase);
+        }
+        #endregion
+
+        #region Delete
+        [Fact]
+        public void Delete_Success_ReturnsNoContent()
+        {
+            var context = CreateInMemoryContext();
+            var controller = CreateController(context);
+            var regResult = controller.Register(new UserDto { Username = "todelete", Password = "StrongP@ssw0rd!" });
+            var user = Assert.IsType<OkObjectResult>(regResult);
+            Assert.NotNull(user.Value);
+            var idProp = user.Value?.GetType().GetProperty("Id");
+            Assert.NotNull(idProp);
+            var idObj = idProp.GetValue(user.Value, null);
+            Assert.NotNull(idObj);
+            var userId = (Guid)idObj;
+            var result = controller.Delete(userId);
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public void Delete_UserNotFound_ReturnsNotFound()
+        {
+            var context = CreateInMemoryContext();
+            var controller = CreateController(context);
+            var missingId = Guid.NewGuid();
+            var result = controller.Delete(missingId);
+            var notFound = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.NotNull(notFound.Value);
+            Assert.Contains("not found", notFound.Value.ToString(), StringComparison.OrdinalIgnoreCase);
+        }
+        #endregion
     }
 }
