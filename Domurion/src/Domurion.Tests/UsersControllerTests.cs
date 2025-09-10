52e6110aadcd13f231e48db8c275c23eecf56e50
eaindome <ekow.a.indome@gmail.com>
2025-09-10 11:57:20 +0000

    add: user controller test cases

--- Domurion/src/Domurion.Tests/UsersControllerTests.cs
+++ Domurion/src/Domurion.Tests/UsersControllerTests.cs
@@ -24,6 +24,7 @@ namespace Domurion.Tests
             return new UsersController(userService);
         }
 
+        #region Register
         [Fact]
         public void Register_Success_ReturnsOk()
         {
@@ -61,5 +62,48 @@ namespace Domurion.Tests
             Assert.NotNull(badRequest.Value);
             Assert.Contains("required", badRequest.Value.ToString());
         }
+        #endregion
+
+        #region Login
+        [Fact]
+        public void Login_Success_ReturnsOk()
+        {
+            var context = CreateInMemoryContext();
+            var controller = CreateController(context);
+            var dto = new UserDto { Username = "loginuser", Password = "StrongP@ssw0rd!" };
+            controller.Register(dto);
+            var result = controller.Login(dto);
+            var ok = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(ok.Value);
+            var username = ok.Value.GetType().GetProperty("Username")?.GetValue(ok.Value, null) as string;
+            Assert.Equal("loginuser", username);
+        }
+
+        [Fact]
+        public void Login_WrongPassword_ReturnsUnauthorized()
+        {
+            var context = CreateInMemoryContext();
+            var controller = CreateController(context);
+            var dto = new UserDto { Username = "loginuser2", Password = "StrongP@ssw0rd!" };
+            controller.Register(dto);
+            var wrongDto = new UserDto { Username = "loginuser2", Password = "WrongPassword1!" };
+            var result = controller.Login(wrongDto);
+            var unauthorized = Assert.IsType<UnauthorizedObjectResult>(result);
+            Assert.NotNull(unauthorized.Value);
+            Assert.Contains("Invalid username or password", unauthorized.Value.ToString());
+        }
+
+        [Fact]
+        public void Login_NonExistentUser_ReturnsUnauthorized()
+        {
+            var context = CreateInMemoryContext();
+            var controller = CreateController(context);
+            var dto = new UserDto { Username = "nouser", Password = "StrongP@ssw0rd!" };
+            var result = controller.Login(dto);
+            var unauthorized = Assert.IsType<UnauthorizedObjectResult>(result);
+            Assert.NotNull(unauthorized.Value);
+            Assert.Contains("Invalid username or password", unauthorized.Value.ToString());
+        }
+        #endregion
     }
 }
