8445f943c02d351fe4d8e5879968993ec5b0db40
eaindome <ekow.a.indome@gmail.com>
2025-09-10 13:49:15 +0000

    add: new migrations

---
+++ Domurion/src/Domurion/Migrations/20250910134605_UpdateUserCredentialModels.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domurion.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserCredentialModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
