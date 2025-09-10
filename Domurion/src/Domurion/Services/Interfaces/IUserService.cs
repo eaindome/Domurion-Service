ebdcecf4ea42b61895ec4c6f993e6189db6054b8
eaindome <ekow.a.indome@gmail.com>
2025-09-10 09:49:00 +0000

    add: update and delete user functionalities

--- Domurion/src/Domurion/Services/Interfaces/IUserService.cs
+++ Domurion/src/Domurion/Services/Interfaces/IUserService.cs
@@ -1,11 +1,13 @@
 using Domurion.Models;
 
-namespace Domurion.Services
+namespace Domurion.Services.Interfaces
 {
     public interface IUserService
     {
         User Register(string username, string password);
         User? Login(string username, string password);
         User? GetByUsername(string username);
+        User UpdateUser(Guid userId, string? newUsername, string? newPassword);
+        void DeleteUser(Guid userId);
     }
 }
