162f9293e168d833b1454e90a54efd89ccdbd519
eaindome <ekow.a.indome@gmail.com>
2025-09-11 08:46:56 +0000

    update: handle navigation to help page

--- client/src/routes/dashboard/+page.svelte
+++ client/src/routes/dashboard/+page.svelte
@@ -120,6 +120,12 @@
     console.log('Navigating to settings');
     setTimeout(() => goto('/settings'), 10);
   }
+
+  function handleHelpClick() {
+    showUserMenu = false;
+    console.log('Navigating to help');
+    setTimeout(() => goto('/help'), 10);
+  }
 </script>
 
 <svelte:head>
@@ -211,7 +217,7 @@
                 <button
                   type="button"
                   class="group flex items-center w-full px-4 py-3 text-sm text-gray-700 hover:bg-gray-50 transition-all duration-150"
-                  on:click={() => { showUserMenu = false; /* handleHelpClick(); */ }}
+                  on:mousedown={handleHelpClick}
                 >
                   <svg class="w-4 h-4 mr-3 text-gray-400 group-hover:text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
