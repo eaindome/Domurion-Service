8b70709e01bff9e098b100d2c08e51d03bfe1034
eaindome <ekow.a.indome@gmail.com>
2025-09-11 12:04:07 +0000

    add: new migrations

---
+++ Domurion/src/Domurion/Migrations/20250911110753_AddUser2FAFields.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domurion.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUser2FAFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AuthProvider",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GoogleId",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "TwoFactorEnabled",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TwoFactorRecoveryCodes",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TwoFactorSecret",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "UserPreferences",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PasswordLength = table.Column<int>(type: "integer", nullable: false),
+                    UseUppercase = table.Column<bool>(type: "boolean", nullable: false),
+                    UseLowercase = table.Column<bool>(type: "boolean", nullable: false),
+                    UseNumbers = table.Column<bool>(type: "boolean", nullable: false),
+                    UseSymbols = table.Column<bool>(type: "boolean", nullable: false),
+                    AutoSaveEntries = table.Column<bool>(type: "boolean", nullable: false),
+                    ShowPasswordStrength = table.Column<bool>(type: "boolean", nullable: false),
+                    SessionTimeoutMinutes = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPreferences", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserPreferences");
+
+            migrationBuilder.DropColumn(
+                name: "AuthProvider",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "GoogleId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TwoFactorEnabled",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TwoFactorRecoveryCodes",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TwoFactorSecret",
+                table: "Users");
+        }
+    }
+}
