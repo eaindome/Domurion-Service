66d62bdaea482212ba271d7fd685ada3aea3ba59
eaindome <ekow.a.indome@gmail.com>
2025-09-12 12:30:01 +0000

    add: new migrations

---
+++ Domurion/src/Domurion/Migrations/20250912122812_NeonMigrations.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domurion.Migrations
+{
+    /// <inheritdoc />
+    public partial class NeonMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Credentials_UserId",
+                table: "Credentials",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Credentials_Users_UserId",
+                table: "Credentials",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Credentials_Users_UserId",
+                table: "Credentials");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Username",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Credentials_UserId",
+                table: "Credentials");
+        }
+    }
+}
