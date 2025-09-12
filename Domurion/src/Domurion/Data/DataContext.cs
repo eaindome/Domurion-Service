991b3a3a1c67dccded8cefb86609a5ac99948338
eaindome <ekow.a.indome@gmail.com>
2025-09-12 04:01:38 +0000

    update: adding constraints to certain tables

--- Domurion/src/Domurion/Data/DataContext.cs
+++ Domurion/src/Domurion/Data/DataContext.cs
@@ -11,5 +11,25 @@ namespace Domurion.Data
         public DbSet<PasswordHistory> PasswordHistories { get; set; }
         public DbSet<UserPreferences> UserPreferences { get; set; }
         public DbSet<SupportRequest> SupportRequests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // User.Username is required and unique
+            modelBuilder.Entity<User>()
+                .Property(u => u.Username)
+                .IsRequired();
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+
+            // Credential.UserId is a required foreign key to User.Id
+            modelBuilder.Entity<Credential>()
+                .HasOne<User>()
+                .WithMany()
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
