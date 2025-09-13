d2bf96252016b0f4009552dafe62b8c8b643b71b
eaindome <ekow.a.indome@gmail.com>
2025-09-13 14:29:26 +0000

    add: working on auto redirect, commented for now

---
+++ client/src/routes/+layout.server.ts
@@ -0,0 +1,14 @@
+// import type { LayoutServerLoad } from './$types';
+// import { redirect } from '@sveltejs/kit';
+
+// export const load: LayoutServerLoad = async ({ locals, url }) => {
+// 	const publicRoutes = ['/login', '/register'];
+// 	const currentPath = url.pathname;
+
+// 	// If not authenticated and not on a public route, redirect to login
+// 	if (!locals.user && !publicRoutes.includes(currentPath)) {
+// 		throw redirect(302, '/login');
+// 	}
+
+// 	return {};
+// };
