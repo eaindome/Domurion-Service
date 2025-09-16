58a25cca2272862417f74941215b3d3dafc6751d
eaindome <ekow.a.indome@gmail.com>
2025-09-16 10:47:11 +0000

    update: send login notifications to google signing

--- Domurion/src/Domurion/Controllers/AuthController.cs
+++ Domurion/src/Domurion/Controllers/AuthController.cs
@@ -48,6 +48,66 @@ namespace Domurion.Controllers
             // Generate JWT for your app with session timeout
             var token = JwtHelper.GenerateJwtToken(user, _configuration, prefs);
 
+            // Send login notification email
+            try
+            {
+                var ip = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+                var time = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss 'UTC'");
+                var userAgent = Request.Headers.UserAgent.ToString();
+                string browser = "Unknown";
+                string os = "Unknown";
+                if (!string.IsNullOrEmpty(userAgent))
+                {
+                    if (userAgent.Contains("Windows")) os = "Windows";
+                    else if (userAgent.Contains("Mac")) os = "MacOS";
+                    else if (userAgent.Contains("Linux")) os = "Linux";
+                    else if (userAgent.Contains("Android")) os = "Android";
+                    else if (userAgent.Contains("iPhone") || userAgent.Contains("iPad")) os = "iOS";
+
+                    if (userAgent.Contains("Chrome")) browser = "Chrome";
+                    else if (userAgent.Contains("Firefox")) browser = "Firefox";
+                    else if (userAgent.Contains("Safari") && !userAgent.Contains("Chrome")) browser = "Safari";
+                    else if (userAgent.Contains("Edge")) browser = "Edge";
+                    else if (userAgent.Contains("MSIE") || userAgent.Contains("Trident")) browser = "Internet Explorer";
+                }
+                // Location info (geo-IP)
+                string city = "Unknown";
+                string country = "Unknown";
+                try
+                {
+                    using var httpClient = new System.Net.Http.HttpClient();
+                    var response = await httpClient.GetStringAsync($"http://ip-api.com/json/{ip}");
+                    var geo = System.Text.Json.JsonDocument.Parse(response);
+                    if (geo.RootElement.GetProperty("status").GetString() == "success")
+                    {
+                        city = geo.RootElement.GetProperty("city").GetString() ?? "Unknown";
+                        country = geo.RootElement.GetProperty("country").GetString() ?? "Unknown";
+                    }
+                }
+                catch { /* Ignore geo-IP errors */ }
+                // URLs
+                var secureAccountUrl = "https://domurion-service.vercel.app/reset-password";
+                var dashboardUrl = "https://domurion-service.vercel.app/dashboard";
+                var placeholders = new Dictionary<string, string>
+                {
+                    { "USER_EMAIL", user.Email },
+                    { "LOGIN_TIME", time },
+                    { "IP_ADDRESS", ip },
+                    { "BROWSER", browser },
+                    { "OPERATING_SYSTEM", os },
+                    { "LOCATION_CITY", city },
+                    { "LOCATION_COUNTRY", country },
+                    { "SECURE_ACCOUNT_URL", secureAccountUrl },
+                    { "DASHBOARD_URL", dashboardUrl }
+                };
+                var templatePath = "Templates/Email/login_notification.html";
+                var emailService = HttpContext.RequestServices.GetService(typeof(Domurion.Helpers.EmailService)) as Domurion.Helpers.EmailService;
+                var subject = "New Login to Your Account";
+                var body = emailService?.RenderTemplate(templatePath, placeholders) ?? string.Empty;
+                emailService?.SendEmail(user.Email, subject, body, isHtml: true);
+            }
+            catch { /* Ignore email errors for login */ }
+
             // Optionally, redirect to frontend with token as query param
             if (!string.IsNullOrEmpty(returnUrl))
                 return Redirect(returnUrl + "?token=" + token);
