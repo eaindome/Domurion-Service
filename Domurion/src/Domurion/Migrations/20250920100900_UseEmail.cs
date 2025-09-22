9510631bbbe2e75fa09879db505c26fc8c103c71
eaindome <ekow.a.indome@gmail.com>
2025-09-22 08:12:06 +0000

    update: new migrations

---
+++ Domurion/src/Domurion/Migrations/20250920100900_UseEmail.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domurion.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Credentials",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Credentials");
+        }
+    }
+}
