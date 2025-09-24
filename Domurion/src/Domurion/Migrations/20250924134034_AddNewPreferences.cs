aee3d5ae522718f4f526d85dee35bbe0e5a86d25
eaindome <ekow.a.indome@gmail.com>
2025-09-24 13:42:04 +0000

    update: new migrations

---
+++ Domurion/src/Domurion/Migrations/20250924134034_AddNewPreferences.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domurion.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewPreferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AutoLockEnabled",
+                table: "UserPreferences",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LoginNotificationsEnabled",
+                table: "UserPreferences",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AutoLockEnabled",
+                table: "UserPreferences");
+
+            migrationBuilder.DropColumn(
+                name: "LoginNotificationsEnabled",
+                table: "UserPreferences");
+        }
+    }
+}
