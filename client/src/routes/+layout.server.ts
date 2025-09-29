efc6a744ad4521d80850345ed237276a36b314c0
eaindome <ekow.a.indome@gmail.com>
2025-09-29 13:27:51 +0000

    update: adding terms and privacy to public rouites

--- client/src/routes/+layout.server.ts
+++ client/src/routes/+layout.server.ts
@@ -2,7 +2,7 @@ import type { LayoutServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
-	const publicRoutes = ['/login', '/register', '/verify', '/message', '/', '/otp'];
+	const publicRoutes = ['/login', '/register', '/verify', '/message', '/', '/otp', '/privacy', '/terms'];
 	const currentPath = url.pathname;
 	const isPublic = publicRoutes.some(route => currentPath.startsWith(route));
 
